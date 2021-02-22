aac7042d1b5c46f6bd8c386df0c389ee2a151f60
CoralCodes <coralhidalgo29@gmail.com>
2021-02-22 23:02:53 +0000

    The bool

--- main.fs
+++ main.fs
@@ -1,14 +1,21 @@
 module Assignment
 
 type AMPM = AM | PM
+ h= T | F
+ m = T| F
 
+
+let time = (h,m,ampm)
 // This function checks if an hour value `h` is not in [1,12] range
-let areHoursInvalid h =
-    false
+let areHoursInvalid h =  h>12 || h< 1 
+
+if areHoursInvalid = false then h->12
 
 // This function checks if a minute value `m` is not in [0,59] range
-let areMinutesInvalid m =
-    false
+let areMinutesInvalid m =  m< 0 || m> 59 
+
+
+if areMinutesInvalid = false then m->0
 
 // This function creates a valid time tuple
 //      use above functions: areHoursInvalid & areMinutesInvalid
