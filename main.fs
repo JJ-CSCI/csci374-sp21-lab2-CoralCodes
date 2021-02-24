a10700991886c752eb3e7d6d2e0674865517e57f
CoralCodes <coralhidalgo29@gmail.com>
2021-02-24 21:51:19 +0000

    Corrected

--- main.fs
+++ main.fs
@@ -1,27 +1,37 @@
 module Assignment
 
 type AMPM = AM | PM
- h= T | F
- m = T| F
 
-
-let time = (h,m,ampm)
 // This function checks if an hour value `h` is not in [1,12] range
 let areHoursInvalid h =  h>12 || h< 1 
 
-if areHoursInvalid = false then h->12
 
 // This function checks if a minute value `m` is not in [0,59] range
-let areMinutesInvalid m =  m< 0 || m> 59 
+let areMinutesInvalid m =  
+  if m< 0 || m> 59  then 
+    true 
+      else 
+        false
 
-
-if areMinutesInvalid = false then m->0
 
 // This function creates a valid time tuple
 //      use above functions: areHoursInvalid & areMinutesInvalid
 let time h m ampm :(int * int * AMPM) =
-    (h, m, ampm)
+    let hour = if areHoursInvalid h then 12 else h
+    
+    let minute = if areMinutesInvalid m then 0 else m
+    (hour, minute, ampm)
 
 // This function compares two times in tuple format
 let lessThan (time1: int * int * AMPM) (time2: int * int * AMPM) :bool =
-    false
+  let h1, m1, p1 = time1  
+  let h2, m2, p2 = time2
+  if p1 < p2 then
+    true
+  else
+    if h1 < h2 then 
+      true 
+    else if m2 < m2 then 
+      true 
+    else 
+      false
